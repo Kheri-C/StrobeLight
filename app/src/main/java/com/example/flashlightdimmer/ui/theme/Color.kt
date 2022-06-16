400165e9f05cbdcae807acc3439543132f3b084d
Kheri-C <88911105+Kheri-C@users.noreply.github.com>
2022-06-16 11:55:26 -0500

    First commit

---
+++ app/src/main/java/com/example/flashlightdimmer/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package com.example.flashlightdimmer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val Orange = Color(0xFFFFA200)
+val Black = Color(0XFF000000)
+val Gray = Color(0xFF888888)
+val DarkGray = Color(0xFF444444)
+val White = Color(0xFFFFFFFF)
